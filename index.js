d6b35b0cca8036bfac9468423c7bdfa1576fc5ef
Merlin <codynott8@gmail.com>
2021-08-02 11:32:26 -0500

    upload

---
+++ index.js
@@ -0,0 +1,168 @@
+new Glide('.glide', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1.90,
+    focusAt:'center',
+    animationDuration: 600,
+    animationTimingFunc: 'linear',
+    peek: {
+        before: 100,
+        after: 100
+      },
+  }).mount()
+
+$("#contact-form").submit(function(){
+    alert("Your contact request has been submitted!")
+})
+
+function initMap(){
+    const styles = [
+        {
+            "featureType": "all",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#000000"
+                },
+                {
+                    "lightness": 13
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#000000"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#144b53"
+                },
+                {
+                    "lightness": 14
+                },
+                {
+                    "weight": 1.4
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#08304b"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#0c4152"
+                },
+                {
+                    "lightness": 5
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#000000"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#0b434f"
+                },
+                {
+                    "lightness": 25
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#000000"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#0b3d51"
+                },
+                {
+                    "lightness": 16
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#000000"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#146474"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#021019"
+                }
+            ]
+        }
+    ]
+    var location = {lat: 38.970564, lng: -94.704444 }
+    var myOptions= {
+        zoom:15,
+        center : new google.maps.LatLng(38.970564,-94.704444),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: styles};
+    var map = new google.maps.Map(document.getElementById("the-map"),myOptions);
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
+}
